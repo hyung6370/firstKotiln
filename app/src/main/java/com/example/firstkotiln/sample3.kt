c2551fc7a20a07fcecdb3183fe71904f36b2e2a2
emptyhead <korea6370@naver.com>
2022-03-13 22:30:32 +0900

    사칙연산(c추가)

--- app/src/main/java/com/example/firstkotiln/sample3.kt
+++ app/src/main/java/com/example/firstkotiln/sample3.kt
@@ -4,13 +4,13 @@ fun main() {
     var input = readLine()!!.split(" ")
     var a = input[0].toInt()
     var b = input[1].toInt()
-    println(a + b)
-    println(a - b)
-    println(a * b)
-    println(a / b)
-    println(a % b)
+    var c = input[2].toInt()
+    println(a + b + c)
+    println(a - b - c)
+    println(a * b * c)
+    println(a / b + c)
+    println(a % b + c)
 
-    println(a * a)
-    println(b * b)
+    //이것은 사칙연산 입니다!!!
 }
 
