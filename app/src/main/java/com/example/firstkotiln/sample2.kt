91d70ca3b2d466fa84809e943c1cb91bd314c912
emptyhead <korea6370@naver.com>
2022-03-13 23:13:47 +0900

    sample2 수정

--- app/src/main/java/com/example/firstkotiln/sample2.kt
+++ app/src/main/java/com/example/firstkotiln/sample2.kt
@@ -4,12 +4,13 @@ fun main() {
     var input = readLine()!!.split(" ")
     var a = input[0].toInt()
     var b = input[1].toInt()
+    var c = input[2].toInt()
 
 
-    println(a+b)
-    println(a-b)
-    println(a*b)
-    println(a%b)
+    println(a+b+c)
+    println(a-b+c)
+    println(a*b*c)
+    println(a%b*c)
 
     println(a * a)
     println(b * b)
