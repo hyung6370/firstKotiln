e40a1114e4ee6d24696da2a14c1330276619910a
emptyhead <korea6370@naver.com>
2022-03-13 22:05:57 +0900

    사칙연산

---
+++ app/src/main/java/com/example/firstkotiln/sample3.kt
@@ -0,0 +1,12 @@
+package com.example.firstkotiln
+
+fun main() {
+    var input = readLine()!!.split(" ")
+    var a = input[0].toInt()
+    var b = input[1].toInt()
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+}
+
