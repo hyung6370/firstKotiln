c82a0081b52a74adc0c2cf5df757408ce1ed20e6
INSU <101244513+PostPlayer@users.noreply.github.com>
2022-03-13 22:38:38 +0900

    수정

--- app/src/main/java/com/example/firstkotiln/sample3.kt
+++ app/src/main/java/com/example/firstkotiln/sample3.kt
@@ -4,12 +4,12 @@ fun main() {
     var input = readLine()!!.split(" ")
     var a = input[0].toInt()
     var b = input[1].toInt()
-    var c = input[2].toInt()
-    println(a + b + c)
-    println(a - b - c)
-    println(a * b * c)
-    println(a / b + c)
-    println(a % b + c)
+
+    println(a + b )
+    println(a - b )
+    println(a * b )
+    println(a / b )
+    println(a % b )
 
     //이것은 사칙연산 입니다!!!
 }
