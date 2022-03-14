78f918ddb15bbaa17240bd38dd30bf900ab8ab76
INSU <101244513+PostPlayer@users.noreply.github.com>
2022-03-14 22:39:26 +0900

    버튼 추가

--- app/src/main/java/com/example/firstkotiln/MainActivity.kt
+++ app/src/main/java/com/example/firstkotiln/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.firstkotiln
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val resultButton = findViewById(R.id.resultButton) as ToggleButton
+        val tt = findViewById(R.id.tex) as TextView
 
+        resultButton.setOnClickListener {
+            if (resultButton.isChecked)
+                tt.text="눌렀습니다"
+            else
+                tt.text="Hello World"
+        }
     }
 }
 
